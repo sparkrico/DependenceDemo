10262aa07e1f42149c85120c49ded17688737474
sparkrico <sparkrico@qq.com>
2018-11-20 17:21:42 +0800

    init

---
+++ mylibrary/src/main/java/sparkrico/com/mylibrary/Test.java
@@ -0,0 +1,19 @@
+package sparkrico.com.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by xiecheng on 2018/11/20
+ */
+public class Test {
+    public void x() {
+        Log.d("zzz", "x from aar");
+        Flowable.just("Hello world")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        System.out.println(s);
+                    }
+                });
+    }
+}

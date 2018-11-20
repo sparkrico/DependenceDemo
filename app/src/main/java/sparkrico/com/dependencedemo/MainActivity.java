aa11a15d6c7b22de10e3cb934eead66bcfb81da1
sparkrico <sparkrico@qq.com>
2018-11-20 17:46:05 +0800

    update app

--- app/src/main/java/sparkrico/com/dependencedemo/MainActivity.java
+++ app/src/main/java/sparkrico/com/dependencedemo/MainActivity.java
@@ -3,11 +3,16 @@ package sparkrico.com.dependencedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import sparkrico.com.mylibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Test test = new Test();
+        test.x();
     }
 }
